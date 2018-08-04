e6f2f7593c5542d2ffd65f29abc27cffbe46869a
thor <sylvain.muller90@gmail.com>
2018-08-04 20:04:07 +0200

    AES128 and b64 encode

--- src/app/routes/public.php
+++ src/app/routes/public.php
@@ -2,15 +2,18 @@
 
   use \Psr\Http\Message\ServerRequestInterface;
   use \Psr\Http\Message\ResponseInterface;
+  use phpseclib\Crypt\AES;
 
   $app->post('/keys', function(ServerRequestInterface $request, ResponseInterface $response) {
       $args = $request->getParsedBody();
-      //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c\\r
-      //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+      $cipher = new AES(AES::MODE_CTR);
+      $cipher->setKey('ABCDEFGHIJ012345');
+      $plaintext = '012345ABCDEFGHIJ';
+      error_log((base64_encode($cipher->encrypt($plaintext))));
+
       $bearer = substr($args['Bearer'], 0, -1);
-      error_log($bearer);
       if ($bearer === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c') {
-          return $response->getBody()->write(json_encode(['key' => 'DZqq88ynCDS51jRahqz2/wAAAAAAAABH'], JSON_UNESCAPED_SLASHES));
+          return $response->getBody()->write(json_encode(['key' => base64_encode($cipher->encrypt($plaintext))], JSON_UNESCAPED_SLASHES));
       }
       return $response->withStatus(401);
   });
