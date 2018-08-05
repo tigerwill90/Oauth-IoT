364f6fcd04184f0482509ffecbf4d4eecad84c0f
thor <sylvain.muller90@gmail.com>
2018-08-05 10:52:27 +0200

    Working first example. v0.1-dev

--- src/app/routes/public.php
+++ src/app/routes/public.php
@@ -4,12 +4,15 @@
   use \Psr\Http\Message\ResponseInterface;
   use phpseclib\Crypt\AES;
 
+
   $app->post('/keys', function(ServerRequestInterface $request, ResponseInterface $response) {
       $args = $request->getParsedBody();
-      $cipher = new AES(AES::MODE_CTR);
+      $cipher = new AES(AES::MODE_ECB); //encryption single
       $cipher->setKey('ABCDEFGHIJ012345');
+      $cipher->disablePadding();
       $plaintext = '012345ABCDEFGHIJ';
-      error_log((base64_encode($cipher->encrypt($plaintext))));
+      error_log('Encrypted : ' . $cipher->encrypt($plaintext));
+      error_log('Encoded : ' . (base64_encode($cipher->encrypt($plaintext))));
 
       $bearer = substr($args['Bearer'], 0, -1);
       if ($bearer === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c') {
