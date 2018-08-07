4d55c80784864db3e53cb6655b2c4bfa49cd6674
thor <sylvain.muller90@gmail.com>
2018-08-07 18:09:19 +0200

    Server SSL with Alias & multiple Bearer detection method (header, body)

---
+++ Dockerfile
@@ -0,0 +1,104 @@
+###
+###             DOCKER LAMP STACK
+###             Start new project with full stack in few minutes
+###
+###
+
+FROM php:apache
+MAINTAINER "tigerwill90" <sylvain.muller90@gmail.com>
+
+ENV USER=daemon
+ENV GROUP=daemon
+
+###
+### Install tools
+###
+RUN set -x \
+        && apt-get update \
+        && apt-get install --no-install-recommends --no-install-suggests -y \
+                git \
+                make \
+                wget
+
+###
+### Install memcached
+###
+RUN set -x \
+        && buildDeps=" \
+                libmemcached-dev \
+                zlib1g-dev \
+        " \
+        && doNotUninstall=" \
+                libmemcached11 \
+                libmemcachedutil2 \
+        " \
+        && apt-get install -y $buildDeps --no-install-recommends \
+        && rm -r /var/lib/apt/lists/* \
+        \
+        && docker-php-source extract \
+        && git clone --branch php7 https://github.com/php-memcached-dev/php-memcached /usr/src/php/ext/memcached/ \
+        && docker-php-ext-install memcached \
+        \
+        && docker-php-source delete \
+        && apt-mark manual $doNotUninstall \
+        \
+        #clean-up
+        && apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false $buildDeps
+
+###
+### Install PDO
+###
+RUN set -x \
+   && docker-php-ext-install pdo_mysql \
+   && docker-php-ext-enable pdo_mysql
+
+###
+### Install composer
+###
+ENV COMPOSER_ALLOW_SUPERUSER=1
+RUN set -x \
+  && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
+  && composer self-update
+
+###
+### Adding custom vhost conf
+###
+ADD /vhost/vhost.conf /etc/apache2/sites-available
+
+###
+### Generate certificat
+###
+RUN openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/ssl-cert-snakeoil.key -out /etc/ssl/certs/ssl-cert-snakeoil.pem -subj "/C=AT/ST=Vienna/L=Vienna/O=Security/OU=Development/CN=example.com"
+
+###
+### Configure ssl 
+###
+RUN a2enmod rewrite
+RUN a2ensite default-ssl
+RUN a2enmod ssl
+
+###
+### Override default vhost conf
+###
+RUN set -x \
+      # disable default vhost conf && enable custom vhost
+      && a2dissite 000-default.conf \
+      && a2ensite vhost.conf \
+      && a2enmod rewrite
+
+###
+### Init project and fix permission
+###
+RUN set -x \
+  && mkdir -p /var/www/html/public \
+  && chmod 0755 /var/www/html/public \
+  && chown ${USER}:${GROUP} /var/www/html/public
+
+RUN service apache2 restart
+
+VOLUME /var/www/html
+
+EXPOSE 80
+EXPOSE 443
+
+WORKDIR /var/www/html
--- src/app/routes/public.php
+++ src/app/routes/public.php
@@ -5,37 +5,62 @@
     use phpseclib\Crypt\AES;
 
     const SHAREDKEY = 'AaBbCcDdEe0123Az';
+
+    $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) : ResponseInterface {
+        $body = $response->getBody();
+        $body->write(json_encode(['status' => 'work in progress']));
+        return $response->withBody($body);
+    });
+
     /**
      * Post route provide
      */
-    $app->post('/keys', function(ServerRequestInterface $request, ResponseInterface $response) : ResponseInterface {
-      $args = $request->getParsedBody();
+    $app->map(['GET', 'POST'],'/keys', function(ServerRequestInterface $request, ResponseInterface $response) : ResponseInterface {
+
+      $headers = $request->getHeader('HTTP_AUTHORIZATION');
+      error_log('headers : ' . json_encode($request->getHeaders()));
+      $bearer = '';
+      if (isset($headers[0])) {
+          if (preg_match('/Bearer\s+(.*)$/i', $headers[0],$matches)) {
+              $bearer =  $matches[1];
+          }
+          else {
+              return $response->withStatus(401);
+          }
+      } else {
+          $args = $request->getParsedBody();
+          if (isset($args['Bearer'])) {
+              $bearer = $args['Bearer'];
+          } else {
+              return $response->withStatus(401);
+          }
+      }
+
       $cipher = new AES(AES::MODE_ECB); //encryption single
       //$cipher = new AES();
 
-      $cipher->setKey('ABCDEFGHIJ012345');
+      $cipher->setKey('abcdef!hij012345');
 
       //$cipher->setIV('0123456789012345');
       $cipher->disablePadding();
       //$plaintext = bin2hex(random_bytes(8));
       $plaintext = SHAREDKEY;
-      error_log('Encrypted : ' . $cipher->encrypt($plaintext));
-      error_log('Encoded : ' . (base64_encode($cipher->encrypt($plaintext))));
+      //error_log('Encrypted : ' . $cipher->encrypt($plaintext));
+      //error_log('Encoded : ' . (base64_encode($cipher->encrypt($plaintext))));
+      error_log('Bearer : ' . $bearer);
 
-      $bearer = substr($args['Bearer'], 0, -1);
       if ($bearer === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c') {
           $body = $response->getBody();
           $body->write(json_encode(['key' => base64_encode($cipher->encrypt($plaintext))], JSON_UNESCAPED_SLASHES));
-          return $response->withBody($body);
+          return $response->withBody($body)->withHeader('content-type', 'application/json');
       }
-      $response = $response->withStatus(401);
 
-      return $response;
+      return $response->withStatus(401);
     });
 
     $app->get('/weather', function(ServerRequestInterface $request, ResponseInterface $response) : ResponseInterface {
         $curl = curl_init('http://192.168.192.80/protected');
-        $authorization = "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        $authorization = 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HEADERFUNCTION, "HandleHeaderLine");
